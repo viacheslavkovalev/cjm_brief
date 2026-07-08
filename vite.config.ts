852c2bec0010466184bf5526c97126cfac612ce5
Viacheslav Kovalev <viacheslavkovalev@MacBook-Pro-Viacheslav.local>
2026-07-08 20:08:34 +0300

    Update white screen

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/brief_sales/",
+  base: "/cjm_brief/",
   plugins: [react()],
 });
